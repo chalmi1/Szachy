5925a7865aa57d70c8848ba56e4505e84641e78d
Michał Chalecki <pijawa2000@gmail.com>
2018-10-20 16:59:10 +0200

    Dodaj pliki projektów.

---
+++ Engine/Board.h
@@ -0,0 +1,37 @@
+#pragma once
+#include "Graphics.h"
+class Board
+{
+private:
+	int width;
+	int height;
+	int cellSize; // 75x75
+	int placex;
+	int placey;
+public:
+	class Position
+	{
+	public:
+		char x;
+		char y;
+		Position() { x = y = '0'; };
+		Position(char a, char b) { x = a; y = b; };
+		bool operator==(Position & a) { if (x == a.x && y == a.y) return true; return false; };
+	};
+
+	Board(Graphics & gfx);
+	void gameOver(Graphics & gfx) const;
+	void start(Graphics & gfx) const;
+	void drawCell(int x, int y, Color c, Graphics & gfx) const;
+	void drawCell(int x, int y, int r, int g, int b, Graphics & gfx) const;
+	void drawCell(Position z, int r, int g, int b, Graphics & gfx) const;
+	void drawBorder(Graphics & gfx) const;
+	void drawSegment(int x, int y, Graphics & gfx, int a = 0);
+	inline int getWidth() const { return width/cellSize; };
+	inline int getHeight() const { return height/cellSize; };
+	//inline int getCellSize() const { return cellSize; };
+	//inline int getPlacex() const { return placex; };
+	//inline int getPlacey() const { return placey; };
+	~Board();
+};
+
---
+++ Engine/FrameTimer.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <chrono>
+class FrameTimer
+{
+private:
+	std::chrono::steady_clock::time_point last;
+public:
+	float Mark();
+	FrameTimer();
+	~FrameTimer();
+};
+
